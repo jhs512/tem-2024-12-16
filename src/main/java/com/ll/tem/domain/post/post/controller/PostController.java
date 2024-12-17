1afb9653788e0b5a8608062c00a6c828cb75851e
jhs512 <jangka512@gmail.com>
2024-12-17 12:19:33 +0900

    BindingResult 2

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -54,6 +54,14 @@ public class PostController {
             @Valid PostWriteForm form,
             BindingResult bindingResult
     ) {
+        if (bindingResult.hasErrors()) {
+            return getFormHtml(
+                    bindingResult.getFieldError().getDefaultMessage(),
+                    form.getTitle(),
+                    form.getContent()
+            );
+        }
+
         return """
                 <h1>글쓰기 완료</h1>
                 
