a98842616d132c70ed1a87deb29825a290e0de04
jhs512 <jangka512@gmail.com>
2024-12-17 12:12:55 +0900

    BindingResult 4

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,7 +51,8 @@ public class PostController {
     @PostMapping("/write")
     @ResponseBody
     public String write(
-            @Valid PostWriteForm form
+            @Valid PostWriteForm form,
+            BindingResult bindingResult
     ) {
         return """
                 <h1>글쓰기 완료</h1>
