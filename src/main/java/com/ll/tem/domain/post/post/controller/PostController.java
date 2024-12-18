205185aaf2559f98d4487bea0182cac6b9abbc51
jhs512 <jangka512@gmail.com>
2024-12-18 10:05:59 +0900

    list 1

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -10,11 +10,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/posts")
@@ -43,23 +41,10 @@ public class PostController {
     }};
 
     @GetMapping
-    @ResponseBody
-    public String showList() {
-        String ul = "<ul>" + posts
-                .reversed()
-                .stream()
-                .map(post -> "<li>%s</li>".formatted(post.getTitle()))
-                .collect(Collectors.joining()) + "</ul>";
+    public String showList(Model model) {
+        model.addAttribute("posts", posts.reversed());
 
-        String body = """
-                <h1>글 목록</h1>
-                
-                %s
-                
-                <a href="/posts/write">글쓰기</a>
-                """.formatted(ul);
-
-        return body;
+        return "domain/post/post/list";
     }
 
 
